886c7e9955ebbb8fed44e8883e647ec16a887b24
Afreed Bin Haque <cuatro.dev@gmail.com>
2023-04-28 03:26:10 +0600

    details design updated

--- resources/views/frontend/pages/website/helping-pages/product-details-preview-model.blade.php
+++ resources/views/frontend/pages/website/helping-pages/product-details-preview-model.blade.php
@@ -1,14 +1,16 @@
 <div class="modal fade" id="product_details_view"  data-bs-backdrop="static" tabindex="-1" style="display: none;" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl">
         <div class="modal-content modal_holder">
-            <div class="modal-header">
-                <h5 class="modal-title">ORSaline-N</h5>
-                <button type="button" class="btn danger" data-bs-dismiss="modal">x</button>
-            </div>
             <div class="modal-body">
                 <div class="row">
+                    <div class="col-12 modal_header_container">
+                            <h5 class="conten_lef">ORSaline-N</h5>
+                            <button type="button" class="btn danger conten_right" data-bs-dismiss="modal">x</button>
+                    </div>
+                </div>
+                <div class="row">
                         <div class="col-md-4 col-sm-12 text-center">
-                            <img class="img-fluid d-flex mx-auto my-4 preview_product_img img_border img_preview_details"
+                            <img class="img-fluid d-flex mx-auto my-4 preview_product_img_detail img_border img_preview_details"
                                 src="http://localhost/pharmacheck/public/assets/img/medicine/1762357035059258.webp" alt="ORSaline-N">
                         </div>
                         <div class="col-md-8 col-sm-12">
@@ -21,7 +23,19 @@
                     <div class="col-12">
                         <div class="card shadow">
                             <div class="card-body product_helper_text">
-                                details
+                                <div class="row">
+                                    <div class="col-12">
+                                        <p class="conten_lef product_details_text">প্রোডাক্ট কিনুন ক্যাশ অন ডেলিভারি বা অনলাইন পেমেন্টে</p>
+                                        <span class="conten_right">
+                                            <p class="product_details_text">পেমেন্ট মেথডঃ</p>
+                                            <ul class="product_details_text">
+                                                <li><img src="{{ asset('assets/img/cod.png') }}" class="payment_method_icon" alt="COD"></li>
+                                                <li><img src="{{ asset('assets/img/bkash.svg') }}" class="payment_method_icon" alt="Bkash"></li>
+                                            </ul>
+                                        </span>
+                                    </div>
+                                    <hr class="section_hr">
+                                </div>
                             </div>
                         </div>
                     </div>
